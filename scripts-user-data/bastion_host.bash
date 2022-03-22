fc853d6f588e54a7f8db00af431cb60b2b63e218
computerSmokio <mavargas@EN2110304.endava.net>
2022-03-22 17:09:10 -0300

    uncomment jenkins

--- scripts-user-data/bastion_host.bash
+++ scripts-user-data/bastion_host.bash
@@ -28,6 +28,7 @@ sudo chef-server-ctl reconfigure --chef-license=accept
 mkdir -p /root/.chef
 sudo chef-server-ctl user-create chefadmin chef admin none@none.com 'abcdefg' --filename /root/.chef/chefadmin.pem
 sudo chef-server-ctl org-create rampup 'rampup_org' --association_user chefadmin --filename /home/ec2-user/.ssh/rampuporg-validator.pem
+chef-server-ctl org-user-add ORG_NAME USER_NAME --admin
 curl https://artifactory-internal.ps.chef.co/artifactory/omnibus-stable-local/com/getchef/chef-workstation/22.2.807/amazon/2/chef-workstation-22.2.807-1.el7.x86_64.rpm -o /tmp/chef-workstation.rpm
 rpm -Uvh /tmp/chef-workstation.rpm
 cd /home/ec2-user
