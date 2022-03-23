8b0202eb7d5dfb17b1763fdd81d6eecf8d8384dd
computerSmokio <mavargas@EN2110304.endava.net>
2022-03-23 09:46:15 -0300

    cambio userdata bastion

--- scripts-user-data/bastion_host.bash
+++ scripts-user-data/bastion_host.bash
@@ -22,16 +22,21 @@ systemctl start jenkins
 systemctl enable jenkins
 hostname chef-infra-server
 #Install Chef Infra Server
+export COOKBOOKS_DIR="/var/lib/jenkins/chef-repo/cookbooks"
+export KNIFE_HOME="/var/lib/jenkins/.chef"
+cat <<EOF | tee /etc/profile.d/variables.sh
+export COOKBOOKS_DIR="/var/lib/jenkins/chef-repo/cookbooks"
+export KNIFE_HOME="/var/lib/jenkins/.chef"
+EOF
+mkdir -p /var/lib/jenkins/.chef
 curl https://artifactory-internal.ps.chef.co/artifactory/omnibus-stable-local/com/getchef/chef-server/14.13.42/amazon/2/chef-server-core-14.13.42-1.el7.x86_64.rpm -o /tmp/chef-server.rpm
 sudo rpm -Uvh /tmp/chef-server.rpm
 sudo chef-server-ctl reconfigure --chef-license=accept
-mkdir -p /var/lib/jenkins/.chef
-sudo chef-server-ctl user-create chefadmin chef admin none@none.com 'abcdefg' --filename /root/.chef/chefadmin.pem
-sudo chef-server-ctl org-create rampup 'rampup_org' --association_user chefadmin --filename /home/ec2-user/.ssh/rampuporg-validator.pem
+sudo chef-server-ctl user-create chefadmin chef admin none@none.com 'abcdefg' --filename $KNIFE_HOME/chefadmin.pem
+sudo chef-server-ctl org-create rampup 'rampup_org' --association_user chefadmin --filename $KNIFE_HOME/rampuporg-validator.pem
 chef-server-ctl org-user-add ORG_NAME USER_NAME --admin
 curl https://artifactory-internal.ps.chef.co/artifactory/omnibus-stable-local/com/getchef/chef-workstation/22.2.807/amazon/2/chef-workstation-22.2.807-1.el7.x86_64.rpm -o /tmp/chef-workstation.rpm
 rpm -Uvh /tmp/chef-workstation.rpm
-export COOKBOOKS_DIR="/var/lib/jenkins/chef-repo/cookbooks"
 curl https://raw.githubusercontent.com/computerSmokio/rampupv2/main/config/config.rb -o /var/lib/jenkins/.chef/config.rb
 git clone https://github.com/computerSmokio/chef-rampup.git /var/lib/jenkins/chef-repo
 echo 'eval "$(chef shell-init bash)"' >> ~/.bash_profile
