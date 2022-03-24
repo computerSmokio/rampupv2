3c0625cac999899178e85552c85aac44c309adcc
computerSmokio <mavargas@EN2110304.endava.net>
2022-03-24 15:29:56 -0300

    cambio de server chef

--- scripts-user-data/worker_node.bash
+++ scripts-user-data/worker_node.bash
@@ -1,8 +1,5 @@
 #!/bin/bash
 echo "${chef_server_ip} chef-infra-server" >> /etc/hosts
 export KUBECONFIG=/etc/kubernetes/admin.conf
-cat <<EOF | tee /etc/profile.d/variables.sh
-export KUBECONFIG=/etc/kubernetes/admin.conf
-EOF
 modprobe br_netfilter
 sysctl -p /etc/sysctl.conf
