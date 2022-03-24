e0623005e8826ba3dd87997c4488f021a633ef16
computerSmokio <mavargas@EN2110304.endava.net>
2022-03-24 11:10:42 -0300

    cambio de server chef

--- scripts-user-data/old_bastion_host.bash
+++ scripts-user-data/old_bastion_host.bash
@@ -20,14 +20,25 @@ curl https://raw.githubusercontent.com/computerSmokio/rampupv2/main/init.groovy.
 systemctl daemon-reload
 systemctl --no-block start jenkins
 systemctl enable jenkins
+hostname chef-infra-server
 #Install Chef Infra Server
-export KNIFE_HOME="/var/lib/jenkins/chef-repo/.chef"
+export COOKBOOKS_DIR="/var/lib/jenkins/chef-repo/cookbooks"
+export KNIFE_HOME="/var/lib/jenkins/.chef"
 cat <<EOF | tee /etc/profile.d/variables.sh
-export KNIFE_HOME="/var/lib/jenkins/chef-repo/.chef"
+export COOKBOOKS_DIR="/var/lib/jenkins/chef-repo/cookbooks"
+export KNIFE_HOME="/var/lib/jenkins/.chef"
 EOF
-git clone https://github.com/computerSmokio/chef-repo.git /var/lib/jenkins/chef-repo
+mkdir -p /var/lib/jenkins/.chef
+curl https://artifactory-internal.ps.chef.co/artifactory/omnibus-stable-local/com/getchef/chef-server/14.13.42/amazon/2/chef-server-core-14.13.42-1.el7.x86_64.rpm -o /tmp/chef-server.rpm
+sudo rpm -Uvh /tmp/chef-server.rpm
+sudo chef-server-ctl reconfigure --chef-license=accept
+sleep(5)
+sudo chef-server-ctl user-create chefadmin chef admin none@none.com 'abcdefg' --filename /var/lib/jenkins/.chef/chefadmin.pem
+sudo chef-server-ctl org-create rampup 'rampup_org' --association_user chefadmin --filename /var/lib/jenkins/.chef/rampuporg-validator.pem
+chef-server-ctl org-user-add rampup chefadmin --admin
 curl https://artifactory-internal.ps.chef.co/artifactory/omnibus-stable-local/com/getchef/chef-workstation/22.2.807/amazon/2/chef-workstation-22.2.807-1.el7.x86_64.rpm -o /tmp/chef-workstation.rpm
 rpm -Uvh /tmp/chef-workstation.rpm
+curl https://raw.githubusercontent.com/computerSmokio/rampupv2/main/config/config.rb -o /var/lib/jenkins/.chef/config.rb
 git clone https://github.com/computerSmokio/chef-rampup.git /var/lib/jenkins/chef-repo
 echo 'eval "$(chef shell-init bash)"' >> ~/.bash_profile
 echo 'export PATH="/opt/chef-workstation/embedded/bin:$PATH"' >> ~/.configuration_file
