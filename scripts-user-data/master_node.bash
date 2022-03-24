3b6eda4f395cb8529ef16b6770e46192462a3b20
computerSmokio <mavargas@EN2110304.endava.net>
2022-03-24 14:42:19 -0300

    cambio de server chef

--- scripts-user-data/master_node.bash
+++ scripts-user-data/master_node.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "${chef_server_ip} chef-infra-server" >> /etc/hosts
-mkdir -p /proc/sys/net/bridge
-echo "1" > /proc/sys/net/bridge/bridge-nf-call-iptables
+modprobe br_netfilter
+sysctl -p /etc/sysctl.conf
