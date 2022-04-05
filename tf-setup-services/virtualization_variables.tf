af68a24fb850be56ec573cfbfd4b4a245df09324
computerSmokio <mavargas@EN2110304.endava.net>
2022-04-05 16:42:14 -0300

    monitoring v0.5

--- tf-setup-services/virtualization_variables.tf
+++ tf-setup-services/virtualization_variables.tf
@@ -31,17 +31,6 @@ locals {
                 chef_server_ip = data.terraform_remote_state.fundation.outputs.chef_ip
             })
             tags = {"Name" = "Worker Node 1"}
-        },
-        {
-            ami = "ami-06078a297452ba5aa"
-            instance_type = "t2.small"
-            subnet_id = data.terraform_remote_state.fundation.outputs.private_subnet_ids[0]
-            security_groups = [module.security_groups.security_groups["worker-node-sg"]]
-            instance_profile = "k8-test-master"
-            user_data = templatefile("./../scripts-user-data/worker_node.bash", {
-                chef_server_ip = data.terraform_remote_state.fundation.outputs.chef_ip
-            })
-            tags = {"Name" = "Worker Node 2"}
         }
     ]
     db_description = {
