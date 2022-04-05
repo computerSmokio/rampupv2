fb1f3bf69d07e33b60c889b8cc878ededbbb0969
computerSmokio <mavargas@EN2110304.endava.net>
2022-04-05 10:20:39 -0300

    monitoring v0.5

--- scripts-user-data/master_node.bash
+++ scripts-user-data/master_node.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "${chef_server_ip} chef-infra-server" >> /etc/hosts
-modprobe br_netfilter
+echo 'net.bridge.bridge-nf-call-iptables=1' | sudo tee -a /etc/sysctl.conf
 sysctl -p /etc/sysctl.conf
