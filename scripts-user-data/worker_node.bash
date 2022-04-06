c0e7af42d2ae1a3d43fd4b50a08d3314d859349e
computerSmokio <mavargas@EN2110304.endava.net>
2022-04-06 12:04:59 -0300

    monitoring v0.5

--- scripts-user-data/worker_node.bash
+++ scripts-user-data/worker_node.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "${chef_server_ip} chef-infra-server" >> /etc/hosts
-hostname ${name}
+hostname ${hostname}
 export KUBECONFIG=/etc/kubernetes/admin.conf
 modprobe br_netfilter
 echo 'net.bridge.bridge-nf-call-iptables=1' | sudo tee -a /etc/sysctl.conf
